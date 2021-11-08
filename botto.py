1320f55ef57742371a7eb9d070ea473e700c5af0
grelade <jacekgrela@gmail.com>
2021-11-08 13:06:49 +0100

    adjustments

--- botto.py
+++ botto.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import logging
 import asyncio
@@ -14,11 +15,11 @@ from crawler import crawler_proc
 from trader import trader_proc
 from data_harvester import harvester_proc
 
-async def main(track_cfg):
+async def main(args):
     try:
         db_args={'database':'cfg/config.db',
                  'isolation_level':None,
-                 'check_same_thread':False}    
+                 'check_same_thread':False}
 
         #cfg = await load_cfg(db_args)
 
@@ -30,48 +31,51 @@ async def main(track_cfg):
 #         elif cfg['general']['logging'] == 'to_screen':
         logging.basicConfig(format=log_format, level=logging.INFO)
 
-        auth = load_auth()    
+        with open(args.track_file,'r') as file:
+            track_cfg = yaml.load(file, Loader=yaml.FullLoader)
+
+        auth = load_auth(file=args.auth_file)
 
         bclient = await create_binance_client(auth['binance_api'],
-                                              auth['binance_secret'])    
+                                              auth['binance_secret'])
 
         tclient = await create_telegram_client(auth['telegram_api_id'],
                                                auth['telegram_api_hash'],
                                                auth['telegram_phone'])
-        
+
         cpu = cpu_proc(client=bclient,
                        db_args=db_args,
                        track_cfg=track_cfg)
-        
+
         agent = agent_proc(client=bclient,
                            db_args=db_args)
-        
+
         crawler = crawler_proc(binance_client=bclient,
                                telegram_client=tclient,
                                db_args=db_args,
                                cfg=track_cfg)
-        
+
         harvester = harvester_proc(client=bclient,
                                    db_args=db_args)
-        
+
         trader = trader_proc(client=bclient,
                              db_args=db_args,
                              cfg=track_cfg)
-        
+
         tasks = []
 
         tasks += [asyncio.create_task(cpu.run())]
-        
+
         tasks += [asyncio.create_task(crawler.run())]
 #         tasks += [asyncio.create_task(crawler.emit_newcoin())]
 
         tasks += [asyncio.create_task(trader.run())]
-        tasks += [asyncio.create_task(trader.update_loop())]        
+        tasks += [asyncio.create_task(trader.update_loop())]
 
         tasks += [asyncio.create_task(harvester.run())]
         tasks += [asyncio.create_task(harvester.emit_data_raw())]
-        tasks += [asyncio.create_task(harvester.run_server())]        
-                
+        tasks += [asyncio.create_task(harvester.run_server())]
+
         tasks += [asyncio.create_task(agent.run())]
         tasks += [asyncio.create_task(agent.emit_order())]
         tasks += [asyncio.create_task(agent.run_server())]
@@ -81,25 +85,26 @@ async def main(track_cfg):
     except asyncio.CancelledError:
 
         await agent.close()
-        await harvester.close() 
+        await harvester.close()
         await trader.close()
         await cpu.close()
         await crawler.close()
-       
+
 
 
 if __name__ == "__main__":
-    
-    if len(sys.argv)>=2:
-        with open(sys.argv[1],'r') as file:
-            track_cfg = yaml.load(file, Loader=yaml.FullLoader)
-    else:
-        print('need track yaml file')   
-        
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--auth_file', default='cfg/auth.yml', type=str)
+    parser.add_argument('--track_file', default='tracks/new_coin_limit.yml', type=str)
+
+    args = parser.parse_args()
+
     loop = asyncio.get_event_loop()
     for sig in (SIGTERM, SIGINT):
         loop.add_signal_handler(sig, error_handler, sig, loop)
-    loop.create_task(main(track_cfg))
+    loop.create_task(main(args))
     loop.run_forever()
     tasks = asyncio.all_tasks(loop=loop)
     for t in tasks:
@@ -107,5 +112,5 @@ if __name__ == "__main__":
     group = asyncio.gather(*tasks, return_exceptions=True)
     loop.run_until_complete(group)
     loop.close()
-    
+
 #     asyncio.run(main())
