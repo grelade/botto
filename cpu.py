34eab33da522e6d7964236abfe4c33dbe37cc2fc
grelade <jacekgrela@gmail.com>
2021-11-15 22:25:43 +0100

    minor modifications

--- cpu.py
+++ cpu.py
@@ -11,7 +11,7 @@ from funcs import estimate_price
 from sockets import order_socket_req, agent_socket_req, harvester_socket_req, crawler_socket_req #outputs
 
 class cpu_proc:
-    
+
     def __init__(self,client,args,**kwargs):
         self.client = client
         self.args = args
@@ -20,7 +20,7 @@ class cpu_proc:
         self.track_cfg = load_track(self.args.track_file)
         self.logger = create_logger(name='cpu')
 #         self.orderq = asyncio.Queue()
-        
+
     async def send_order(self,request):
         with order_socket_req() as sock:
             await sock.send_json(request)
@@ -32,42 +32,42 @@ class cpu_proc:
 #             while True:
 #                 request = await self.orderq.get()
 #                 await sock.send_json(request)
-#                 response = await sock.recv_json() 
-        
+#                 response = await sock.recv_json()
+
     async def add_harvester(self,request):
         with harvester_socket_req() as sock:
             await sock.send_json(request)
             response = await sock.recv_json()
             return response
-        
+
     async def add_agent(self,request):
         with agent_socket_req() as sock:
             await sock.send_json(request)
             response = await sock.recv_json()
             return response
-        
+
     async def add_crawler(self,request):
         with crawler_socket_req() as sock:
             await sock.send_json(request)
             response = await sock.recv_json()
             return response
-                
+
     async def coin_track(self):
         self.logger.info('init coin track...')
 
         rec = {'symbol': self.track_cfg['symbol'],
                'side': SIDE_BUY}
-        
+
         #new order flags
         is_s = 'symbol' in self.track_cfg.keys()
         is_tot = 'trader_order_type' in self.track_cfg.keys()
         is_op = 'order_price' in self.track_cfg.keys()
         is_p = 'price' in self.track_cfg.keys()
-        
-        
+
+
         #existing order flags
         is_oi = 'init_order_id' in self.track_cfg.keys()
-        
+
         #new order
         if is_s and is_tot and is_op:
             rec['mode'] = 'new'
@@ -81,38 +81,38 @@ class cpu_proc:
                 rec['price'] = self.track_cfg['price']
             else:
                 raise Exception('unknown order / check conf file')
-        
+
         #existing order
         elif is_s and is_oi:
             rec['mode'] = 'existing'
             rec['orderId'] = self.track_cfg['init_order_id']
         else:
             raise Exception('unknown order / check conf file')
-            
+
         resp_o = await self.send_order(rec)
-        
+
         req_h = {'name':self.track_cfg['harvester_name'],
                  'symbol':self.track_cfg['symbol'],
                  'active':True,
                  'running':False}
         resp_h = await self.add_harvester(req_h)
         if resp_h['resp'] != RESPONSE_OK: raise Exception('harvester problem')
-            
+
         req_a = {'name':self.track_cfg['agent_name'],
                  'init_order_id':resp_o['id'],
                  'harvester_id':resp_h['harvester_id'],
                  'type':self.track_cfg['agent_type'],
                  'params':self.track_cfg['agent_params'],
                  'active':True,
-                 'running':False}    
+                 'running':False}
         resp_a = await self.add_agent(req_a)
         if resp_a['resp'] != RESPONSE_OK: raise Exception('agent problem')
-            
+
         self.logger.info('end coin track...')
 
     async def newcoin_track(self):
         self.logger.info('init newcoin track...')
-        
+
         #crawl
         req_c = {'mock':self.track_cfg['crawler_mock_msg']}
         resp_c = await self.add_crawler(req_c)
@@ -126,39 +126,48 @@ class cpu_proc:
                'order_price': self.track_cfg['order_price'],
                'mode': 'new',
                'mock': self.track_cfg['trader_mock_orders']}
-        
+
         if self.track_cfg['trader_order_type'] == ORDER_TYPE_MARKET:
             rec['type']= ORDER_TYPE_MARKET
-            
+
         elif self.track_cfg['trader_order_type'] == ORDER_TYPE_LIMIT:
             #last_price = await self.get_last_price(newcoin['symbol'])
             rec['type'] = ORDER_TYPE_LIMIT
             rec['price'] = self.track_cfg['trader_price_frac']*newcoin['est_init_price']
+        
+        success = False
+        while not success:
+            try:
+                await asyncio.sleep(0.001)
+                resp_o = await self.send_order(rec)
+                success = True
+            except BinanceAPIException as e:
+                if e.code == BINANCE_WRONG_SYMBOL:
+                    self.logger.info('no symbol yet')
+                    pass
 
-        resp_o = await self.send_order(rec)
-    
         req_h = {'name':self.track_cfg['harvester_name'],
                  'symbol':newcoin['symbol'],
                  'active':True,
                  'running':False}
         resp_h = await self.add_harvester(req_h)
         if resp_h['resp'] != RESPONSE_OK: raise Exception('harvester problem')
-            
+
         req_a = {'name':self.track_cfg['agent_name'],
                  'init_order_id':resp_o['id'],
                  'harvester_id':resp_h['harvester_id'],
                  'type':self.track_cfg['agent_type'],
                  'params':self.track_cfg['agent_params'],
                  'active':True,
-                 'running':False}    
+                 'running':False}
         resp_a = await self.add_agent(req_a)
         if resp_a['resp'] != RESPONSE_OK: raise Exception('agent problem')
-            
+
         self.logger.info('end newcoin track...')
-    
+
     async def backlog_track(self):
         self.logger.info('init backlog track...')
-        
+
         req_h = {'name':self.track_cfg['harvester_name'],
                  'symbol':newcoin['symbol'],
                  'active':True,
@@ -166,7 +175,7 @@ class cpu_proc:
                  'backlog_file':self.track_cfg['backlog_file']}
         resp_h = await self.add_harvester(req_h)
         self.logger.info('end backlog track...')
-        
+
     async def order_preparation(self,newcoin):
         nc = newcoin.copy()
         tz = pytz.timezone(self.cfg['general']['timezone'])
@@ -178,13 +187,13 @@ class cpu_proc:
         #t_start = tz.localize(datetime.fromtimestamp(newcoin['start_time']))
         t_price_estimate = t_start - timedelta(minutes=5)
         t_end_preparation = t_start - timedelta(minutes=1)
-        
-        self.logger.info(f'init price estimation...')   
-        
+
+        self.logger.info(f'init price estimation...')
+
         #wait with price estimation
         while (t_now:= tz.localize(datetime.now())) < t_price_estimate:
             await asyncio.sleep(.001)
-            
+
         auth = load_auth(self.args.auth_file)
         prices = await estimate_price(auth['coinapi_apikey'],
                                      base_asset=newcoin['coin_name'],
@@ -192,19 +201,22 @@ class cpu_proc:
                                      time_end=t_price_estimate,
                                      quote_asset=self.pair,
                                      period='5MIN')
-        
+
         nc['est_init_price'] = prices['price_average']
-        
+
         while (t_now := tz.localize(datetime.now())) < t_end_preparation:
             await asyncio.sleep(.001)
         self.logger.info(f'... {nc["symbol"]} price = {nc["est_init_price"]}')
+
+        while (t_now := tz.localize(datetime.now())) < t_start:
+            await asyncio.sleep(.000001)
         return nc
 
     async def get_last_price(self,symbol):
         last_price = await self.client.get_symbol_ticker(symbol=symbol)
         last_price = float(last_price['price'])
-        return last_price            
-            
+        return last_price
+
     async def run_server(self):
         self.logger.info('Starting cpu_proc()')
         if self.track_cfg['track_mode'] == 'coin':
@@ -215,18 +227,18 @@ class cpu_proc:
             await self.backlog_track()
         else:
             self.logger.info('unknown track_mode')
-        
+
     async def close(self):
         print('cpu close')
         await self.client.close_connection()
-        
+
 async def main(args):
     try:
-        
-        auth = load_auth(args.auth_file)    
+
+        auth = load_auth(args.auth_file)
 
         bclient = await create_binance_client(auth['binance_api'],
-                                              auth['binance_secret'])    
+                                              auth['binance_secret'])
 
         proc = cpu_proc(client = bclient,
                         args = args)
@@ -238,12 +250,12 @@ async def main(args):
 
     except asyncio.CancelledError:
         await proc.close()
-            
+
 
 if __name__ == "__main__":
-    
+
     args = set_argparser()
-    
+
     loop = asyncio.get_event_loop()
     for sig in (SIGTERM, SIGINT):
         loop.add_signal_handler(sig, error_handler, sig, loop)
@@ -255,5 +267,5 @@ if __name__ == "__main__":
     group = asyncio.gather(*tasks, return_exceptions=True)
     loop.run_until_complete(group)
     loop.close()
-    
+
 #     asyncio.run(main())
--- crawler.py
+++ crawler.py
@@ -16,7 +16,7 @@ from funcs import extract_url
 from sockets import crawler_socket_rep
 
 class crawler_proc:
-    
+
     def __init__(self,binance_client,telegram_client,args):
         self.bclient = binance_client
         self.tclient = telegram_client
@@ -25,11 +25,12 @@ class crawler_proc:
         self.db_args = self.cfg['db']
         self.scan_interval = self.cfg['crawler']['scan_interval']
         self.channel_url = self.cfg['crawler']['channel_url']
+        self.init_msg = self.cfg['crawler']['init_telegram_msg']
         self.local_timezone = self.cfg['general']['timezone']
         self.pairing = self.cfg['general']['pair']
         self.logger = create_logger(name='crawler')
         self.mock_msg = True
-        
+
     # new_coins crawler
     async def is_msg_about_new_coin(self, msg_dict: dict) -> dict:
         msg = msg_dict['message']
@@ -57,31 +58,31 @@ class crawler_proc:
             time_dt_tz = tz.localize(time_dt)
 
             time_dt_tz_local = time_dt_tz.astimezone(tz_local)
-            
+
             msg_time_local = msg_dict['date'].astimezone(tz_local)
-            
+
             return {'coin_name': coin_name,
                     'symbol': coin_name+self.pairing,
                     'start_time': time_dt_tz_local.isoformat(),
                     'msg_time': msg_time_local.isoformat(),
                     #'start_time': int(time_dt_tz_local.timestamp()),
                     #'msg_time': int(msg_time_local.timestamp()),
-                   }   
-        
+                   }
+
     async def crawl_loop(self,sock):
-        
+
         while True:
 
             if not self.mock_msg:
                 msg_channel = await self.tclient.get_entity(self.channel_url)
-                newest_msg = (await self.tclient.get_messages(msg_channel,limit=1))[0].to_dict()
+                newest_msg = (await self.tclient.get_messages(msg_channel,limit=self.init_msg+1))[-self.init_msg].to_dict()
 #                 newest_msg = (await self.tclient.get_messages(msg_channel,limit=50))[29].to_dict()
 #                 for i,m in enumerate(await self.tclient.get_messages(msg_channel,limit=50)):
 #                     print(i,m.to_dict()['message'])
             else:
                 self.new_msg_id = 0
                 #mock message
-                newest_msg = {'id': 3270, 
+                newest_msg = {'id': 3270,
                               'date': datetime(2021, 10, 5, 2, 39, 36, tzinfo=timezone.utc),
                               'message': 'Binance Will List Adventure Gold (AGLD) in the Innovation Zone\nhttps://www.binance.com/en/support/announcement/4a0b5201a6874cfe98adad4f74b6a75b'}
 
@@ -99,8 +100,8 @@ class crawler_proc:
                     self.logger.info('... but NOT about a new coin.')
             else:
                 self.logger.info('no new msg.')
-            await asyncio.sleep(self.scan_interval)        
-        
+            await asyncio.sleep(self.scan_interval)
+
     async def run_server(self):
         self.logger.info('Starting crawler_proc()')
         with crawler_socket_rep() as sock:
@@ -108,7 +109,7 @@ class crawler_proc:
                 while True:
                     self.new_msg_id = 0
                     crawl_request = await sock.recv_json()
-                    
+
                     self.mock_msg = crawl_request['mock']
                     mockstr = 'REAL'
                     if self.mock_msg:
@@ -120,13 +121,13 @@ class crawler_proc:
             except FloodWaitError as e:
                 self.logger.info(e)
                 await asyncio.sleep(60)
-                pass        
+                pass
 
     async def close(self):
         print('crawler close')
-        await self.bclient.close_connection()    
-        await self.tclient.close_connection()    
-        
+        await self.bclient.close_connection()
+        await self.tclient.close_connection()
+
 async def main(args) -> None:
     try:
 
@@ -134,7 +135,7 @@ async def main(args) -> None:
 
         bclient = await create_binance_client(auth['binance_api'],
                                               auth['binance_secret'])
-        
+
         tclient = await create_telegram_client(auth['telegram_api_id'],
                                                auth['telegram_api_hash'],
                                                auth['telegram_phone'])
@@ -142,19 +143,19 @@ async def main(args) -> None:
         proc = crawler_proc(binance_client = bclient,
                             telegram_client = tclient,
                             args = args)
-        
+
         tasks = []
         tasks += [asyncio.create_task(proc.run_server())]
 
         await asyncio.gather(*tasks)
-        
+
     except asyncio.CancelledError:
         await proc.close()
-        
+
 if __name__ == "__main__":
 
     args = set_argparser()
-    
+
     loop = asyncio.get_event_loop()
     for sig in (SIGTERM, SIGINT):
         loop.add_signal_handler(sig, error_handler, sig, loop)
@@ -166,6 +167,5 @@ if __name__ == "__main__":
     group = asyncio.gather(*tasks, return_exceptions=True)
     loop.run_until_complete(group)
     loop.close()
-    
+
 #     asyncio.run(main())
-    
